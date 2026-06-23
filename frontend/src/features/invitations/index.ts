870edb79dd99668ba40f735c719ae2acd032effe
pricetula <pricetula@gmail.com>
2026-06-23 15:11:33 +0300

    remove unused feautre

--- frontend/src/features/invitations/index.ts
+++
@@ -1,3 +0,0 @@
-export { InvitationTable } from "./components/invitation-table";
-export { InviteFormDialog } from "./components/invite-form";
-export { useInvitations, useCreateInvitations } from "./hooks/use-invitations";
